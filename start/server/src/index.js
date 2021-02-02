7c46c1472316672eec9323245ea0a948c2448523
Han Wu <xjohnwu@gmail.com>
2021-02-02 14:43:06 +0800

    tutorial 1-3

--- start/server/src/index.js
+++ start/server/src/index.js
@@ -1,1 +1,27 @@
 require('dotenv').config();
+const { ApolloServer } = require('apollo-server');
+const typeDefs = require('./schema');
+const { createStore } = require('./utils');
+const resolvers = require('./resolvers');
+
+const LaunchAPI = require('./datasources/launch');
+const UserAPI = require('./datasources/user');
+
+const store = createStore();
+
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+  dataSources: () => ({
+    launchAPI: new LaunchAPI(),
+    userAPI: new UserAPI({ store })
+  })
+});
+
+server.listen().then(() => {
+  console.log(`
+      Server is running!
+      Listening on port 4000
+      Explore at https://studio.apollographql.com/dev
+    `);
+});
